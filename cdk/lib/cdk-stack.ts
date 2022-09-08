03cd5bc9db8dedffa9174aec2e72ea4e9f50a7f3
amarkumarsbg <amarkumarsbg13@gmail.com>
2022-09-08 15:20:10 +0530

    changed cdk config

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -9,11 +9,11 @@ export class CdkStack extends cdk.Stack {
       serviceName: "my-apprunner-app",
       sourceConfiguration: {
         authenticationConfiguration: {
-          connectionArn: "arn:aws:apprunner:us-west-2:885121665536:connection/apprunnerGithubConnector/7f215ea722b241e2bd47ffbc67a3d048",
+          connectionArn: "arn:aws:apprunner:us-east-1:219841394426:connection/apprunnerGithubConnector/60a5f2ec853c4babb25cd323e00ce7fc",
         },
         autoDeploymentsEnabled: true,
         codeRepository: {
-          repositoryUrl: "https://github.com/mjzone/cdk-apprunner-demo",
+          repositoryUrl: "https://github.com/amarkumarsbg/AWS-App-Runner",
           sourceCodeVersion: {
             type: "BRANCH",
             value: "main",
